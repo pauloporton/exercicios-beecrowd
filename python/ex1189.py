072d6c44662b37f859506013b5cb41521567e60d
Paulo Porto <paulotrop20@gmail.com>
2025-06-25 19:17:39 -0300

    exercicios

---
+++ python/ex1189.py
@@ -0,0 +1,24 @@
+O = input()
+
+matriz = []
+
+for _ in range(12):
+    linha = []
+    for _ in range(12):
+        valor = float(input())
+        linha.append(valor)
+    matriz.append(linha)
+    
+soma = 0
+contagem = 0
+
+for i in range(12):
+    for j in range(12):
+        if j < i and j < 11 - i:
+            soma += matriz[i][j]
+            contagem += 1
+        
+if  O == 'S':
+    print(f'{soma:.1f}')
+else:
+    print(f'{soma/contagem:.1f}')
